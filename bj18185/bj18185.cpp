e0e201b9a775a97f5348667ad9bab6ece0572a03
PTFDB <kbs95123@gmail.com>
2022-11-25 15:24:04 +0900

    bj11815해답 보며 이해

--- bj18185/bj18185.cpp
+++ bj18185/bj18185.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
+#include<memory.h>
 // using namespace std; 를 사용하지 않은 이유
 // 프로그램이 복잡해질수록 오류 초래
 using std::cin;
 using std::cout;
-
-// 집주변 N(3~10000) 라면공장
-// 각 공장(3~N) 까지 차례대로 번호 부여 
-// 교준이 i(0~10000)번 공장 Ai(0~10000) 개의 라면을 만들고자함 
-// 1.i번 공장에서 라면을 하나 구매 - 3원
-// 2.i번 공장과 (i+1)번 공장에서 각각 라면을 하나씩 구매한다(1 ≤ i ≤ N-1) - 5원
-// 3.i번 공장과 (i+1)번 공장, (i+2)번 공장에서 각각 라면을 하나씩 구매한다(1 ≤ i ≤ N-2) - 7원
-// 최소의 비용으로 라면을 구매하고자 할때 교준이가 필요한 금액을 출력하는 프로그램
-//
-
+using std::ios;
+using std::min;
 int main() {
-	int facNum;
-
-	cin >> facNum;
-
-	// 동적할당 buy_1....
-	int* pbuyarray = new int[facNum];
-	
-	for (int i = 0; i < facNum; i++)
-	{
-		cin >> pbuyarray[i];
+	// 입출력 속도 높이기 위해 사용
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int n, ans=0;
+	int v[100001];
+	cin >> n;
+	for (int i = 1; i <= n; i++){
+		cin >> v[i];
 	}
+	for (int i = 1; i <= n; i++){
+		if (v[i + 1] > v[i + 2]) {
+			// min함수 두 값중 최소값 반환
+			// Q 이과정 이해 안감
+			int a = min(v[i], v[i + 1] - v[i + 2]);
+			ans += 5 * a;
+			v[i] -= a; 
+			v[i + 1] -= a;
 
+			int b = min(v[i], min(v[i + 1], v[i + 2]));
+			ans += 7 * b;
+			v[i] -= b;
+			v[i + 1] -= b;
+			v[i + 2] -= b;
+		}
+		else {
+			int b = min(v[i], min(v[i + 1], v[i + 2]));
+			ans += 7 * b;
+			v[i + 1] -= b;
+			v[i + 2] -= b;
 
-
+			int c = min(v[i], v[i + 1]);
+			ans += 5 * c;
+			v[i + 1] -= c;
+		}
+		ans += 3 * v[i];
+	}
+	cout << ans << "\n";
 	return 0;
 }
 
